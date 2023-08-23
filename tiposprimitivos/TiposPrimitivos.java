71733879310f6d82c903e162e21efbe55b91fe4e
Johnner Yelcde <johnnercastrojy@gmail.com>
2023-08-23 12:55:58 -0300

    Tipos primitivos em Java

---
+++ tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,25 @@
+package tiposprimitivos;
+
+public class TiposPrimitivos {
+    public static void main(String[] args){
+
+    }
+}
+
+
+/* 
+Familia		Tipo Primitivo		Classe Invólucro	Tamanho		    Exemplo
+
+Lógico      boolean             Boolean			    1 bit		    true
+
+Literais	char;			    Character		    1 byte		    'A' Aspas simples
+		    -			        String			    1 byte/cada	    "Java" Aspas Duplas
+
+Interiros	byte;			    Byte			    1 byte		    127
+		    short;			    Short			    2 bytes		    32.767
+		    int;			    Integer			    4 bytes		    2.147.483
+		    long			    Long			    8 bytes		    2^63
+
+Reais		float;			    Float			    4 bytes		    3.4e^+38
+		    double			    Double			    8 bytes		    1.8e^+308
+ */
